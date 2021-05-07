1be3bb8c49cb6406d409279ffa00a6b67c1fa53b
ongomobile <ongomobile@gmail.com>
2021-05-07 15:09:18 -0700

    Initial Commit

---
+++ Firebase-SwiftUI-MVVM/Firebase_SwiftUI_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_SwiftUI_MVVMApp.swift
+//  Firebase-SwiftUI-MVVM
+//
+//  Created by Michael Haslam on 5/7/21.
+//
+
+import SwiftUI
+
+@main
+struct Firebase_SwiftUI_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Firebase-SwiftUI-MVVMTests/Firebase_SwiftUI_MVVMTests.swift
@@ -0,0 +1,33 @@
+//
+//  Firebase_SwiftUI_MVVMTests.swift
+//  Firebase-SwiftUI-MVVMTests
+//
+//  Created by Michael Haslam on 5/7/21.
+//
+
+import XCTest
+@testable import Firebase_SwiftUI_MVVM
+
+class Firebase_SwiftUI_MVVMTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
