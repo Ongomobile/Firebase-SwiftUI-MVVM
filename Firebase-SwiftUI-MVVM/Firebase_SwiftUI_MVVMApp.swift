4433a6ad515b188240524b57f98e7813838940e3
ongomobile <ongomobile@gmail.com>
2021-05-07 15:37:44 -0700

    Setup Firebase

--- Firebase-SwiftUI-MVVM/Firebase_SwiftUI_MVVMApp.swift
+++ Firebase-SwiftUI-MVVM/Firebase_SwiftUI_MVVMApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct Firebase_SwiftUI_MVVMApp: App {
@@ -13,5 +14,12 @@ struct Firebase_SwiftUI_MVVMApp: App {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
     }
 }
