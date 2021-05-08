f3828ce9819246cace93a028ccf8e21cf0ca120f
ongomobile <ongomobile@gmail.com>
2021-05-07 17:08:46 -0700

    Add StoryViewModel

---
+++ Firebase-SwiftUI-MVVM/ViewModels/StoryViewModel.swift
@@ -0,0 +1,37 @@
+//
+//  StoryViewModel.swift
+//  ThaiStories
+//
+//  Created by Michael Haslam on 3/2/21.
+//
+
+import Foundation
+import Combine
+
+class StoryViewModel: ObservableObject, Identifiable {
+    private let storyRepository = StoryRepository()
+    @Published var story: Story
+    
+    private var cancellables: Set<AnyCancellable> = []
+    
+    var id = ""
+    
+    init(story: Story) {
+        self.story = story
+        
+        // set up binding for story between the stories id and the viewmodels id then store object in cancellables so it can be canceled later
+        $story
+            .compactMap { $0.id }
+            .assign(to: \.id, on: self)
+            .store(in: &cancellables)
+    }
+    
+    func remove() {
+        storyRepository.remove(story)
+    }
+    
+    func editStory(story: Story) {
+        storyRepository.updateStory(story)
+    }
+    
+}
